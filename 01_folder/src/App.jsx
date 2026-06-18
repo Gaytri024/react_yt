4bbc335e3632030fbb489462782dcc31ddd16af8
gaytri gautam <gaytrigautam425@gmail.com>
2026-06-18 19:20:23 +0530

    01_folder done

---
+++ 01_folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1 id='dad'>Hello</h1>
+      <p id='chacha'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur enim autem vero maiores 
+        corporis repudiandae dolorem aliquam laboriosam illo id molestias minus, impedit non nisi sunt 
+        facilis? Fugiat, voluptatem earum.</p>
+    </div>
+    </>
+  )
+}
+
+export default App
