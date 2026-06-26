80905e4c775e2ab1e49fc5cf132557d4d2c13673
gaytri gautam <gaytrigautam425@gmail.com>
2026-06-26 14:36:53 +0530

    Added 09_useState

---
+++ 09_useState/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { use } from 'react'
+import { useState } from 'react'
+
+const App = () => {
+  const [num, setnum] = useState(0)
+
+  function increaseNum() {
+    setnum(num + 1)
+  }
+
+  function decreaseNum() {
+    setnum(num - 1)
+  }
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={increaseNum}>Increase</button>
+      <button onClick={decreaseNum}>Decrease</button>
+    </div>
+  )
+}
+
+export default App
